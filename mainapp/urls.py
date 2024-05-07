f3e121e63dc87e3aab617ed62b6ab440f40f5473
lester wee <lester.wee.68@gmail.com>
2024-05-07 22:49:05 +0800

    add models and migrated. add urls.py file in mainapp.

---
+++ mainapp/urls.py
@@ -0,0 +1,8 @@
+# This file contains the URL patterns specific to the 'mainapp' app. It defines how URLs associated with this app should be handled.
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # define app specific URLs here
+]
