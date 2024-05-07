1714bf12d47a5960da9bf6a4d1c21f21a5ed2d12
lester wee <lester.wee.68@gmail.com>
2024-05-07 23:08:52 +0800

    add signup logic in views and forms.py

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+import requests
+from django.contrib import auth, messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from django.contrib.auth.tokens import default_token_generator
+from django.core.exceptions import PermissionDenied
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseNotAllowed
+from django.shortcuts import redirect, render
+from django.template.defaultfilters import slugify
+from django.utils.http import urlsafe_base64_decode
 
-# Create your views here.
+from .forms import CustomUserCreationForm
+from .models import Cryptocurrency, Portfolio, Profile, Referal
